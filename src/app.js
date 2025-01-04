f932712322dacbf656812136198aa38d6cade91f
felix eugenio mavila <felixmacarenco@gmail.com>
2025-01-04 01:24:04 +0100

    feat: add consign

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 /* eslint-disable no-unused-vars */
 const express = require('express');
 const app = express();
+const consign = require('consign');
+
+consign()
+    .then('src/config/middlewares.js')
+    .into(app);
 
 app.get('/', (req, res) => {
     res.status(200).send('Hello World!');
