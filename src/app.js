8c4b502dd1d4f5498ecc20ac3a64239c6fde1c2f
felix eugenio mavila <felixmacarenco@gmail.com>
2025-01-04 01:05:51 +0100

    fix: Error: expect(received).toBe(expected) // Object.is equality

--- src/app.js
+++ src/app.js
@@ -1,8 +1,24 @@
+/* eslint-disable no-unused-vars */
 const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
     res.status(200).send('Hello World!');
+})
+
+app.get('/users', (req, res) => {
+    const users = [
+    {
+        name:'Guilherme',
+        mail:'Guilherme@gmail.com'
+
+    }]
+    res.status(200).json(users)
+})
+
+app.post('/users', (req, res) => {
+    const user = req.body;
+    res.status(201).json(user)
 })
 
 module.exports = app
---
+++ test/user.test.js
@@ -0,0 +1,21 @@
+/* eslint-disable no-undef */
+const request = require('supertest');   
+const app = require('../src/app');
+
+test('Deve listar todos os usuários',() => {
+    return request(app).get('/users').then(response => {
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveLength(1);
+        //expect(response.body[0].name).toHaveProperty('name', 'Guilherme');
+    })
+})
+
+test('Deve criar um usuário',() => {
+    return request(app).post('/users').send({
+        name: 'Guilherme',
+        mail: 'Guilherme@gmail.com'
+    }).then(response => {
+        expect(response.status).toBe(201);
+        expect(response.body.name).toBe('Guilherme');
+    })
+})
